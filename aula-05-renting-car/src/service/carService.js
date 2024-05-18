ef5e41ce6054d78f03fc946747e866ffab32db2e
guiziis <guigamoda@hotmail.com>
2024-05-18 20:21:54 -0300

    test: termina os testes

--- aula-05-renting-car/src/service/carService.js
+++ aula-05-renting-car/src/service/carService.js
@@ -1,8 +1,26 @@
+const Tax = require("../entites/tax");
+const Transaction = require("../entites/transaction");
 const BaseRepository = require("../repository/base/baseRespository");
 
 class CarService {
   constructor({cars}) {
     this.carRepository = new BaseRepository({file: cars})
+    this.taxesBasedOnAge = Tax.taxesBasesOnAge
+    this.currencyFormat = new Intl.NumberFormat('pt-br', {
+      style: 'currency',
+      currency: 'BRL'
+    })
+  }
+
+  calculateFinalPrice(customer, carCategory, numberOfDays) {
+    const {age} = customer
+    const {price} = carCategory
+    const {then: tax} = this.taxesBasedOnAge.find(tax => age >= tax.from && age <= tax.to)
+
+    const finalPrice = ((tax * price) * (numberOfDays))
+    const formattedPrice = this.currencyFormat.format(finalPrice)
+
+    return formattedPrice
   }
 
   async getAvailableCars(carsCategory) {
@@ -21,6 +39,20 @@ class CarService {
   getRandomPositionFromArray(list) {
     const randomNumber = Math.floor(Math.random() * list.length)
     return randomNumber
+  }
+
+  async rent(customer, carCategory, numberOfDays) {
+    const car = await this.getAvailableCars(carCategory)
+    const finalPrice = this.calculateFinalPrice(customer, carCategory, numberOfDays)
+
+    const today = new Date()
+    today.setDate(today.getDate() + numberOfDays)
+    const options = {year: 'numeric', month: 'long', day: 'numeric'}
+    const dueDate = today.toLocaleDateString('pt-br', options)
+
+    const transaction = new Transaction({customer, car, amount: finalPrice, dueDate})
+  
+    return transaction
   }
 
 }
